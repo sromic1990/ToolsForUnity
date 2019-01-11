83d432d3803904179588374c6776b31265f53fe8
Sourav Chatterjee <sromic@gmail.com>
2019-01-11 13:02:08 +0530

    [Update] Added Camera orthographic utilities

---
+++ Assets/Sourav/Utilities/Scripts/Camera/CameraFollow/CameraFollow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SwingKing._Scripts.ControllerRelated;
+using SwingKing._Scripts.Element;
+using UnityEngine;
+
+namespace Sourav.Utilities.Scripts.Camera.CameraFollow
+{
+	[RequireComponent(typeof(UnityEngine.Camera))]
+	public class CameraFollow : Controller
+	{
+		private UnityEngine.Camera _camera;
+		[SerializeField] private Transform _target;
+		[Tooltip("Put between 0 and 1")]
+		[SerializeField] private float _cameraSmoothSpeed = 0.125f;
+		[SerializeField] private Vector3 _offset;
+
+		private bool zoom;
+		private bool runScript;
+
+		private void Awake()
+		{
+			_camera = GetComponent<UnityEngine.Camera>();
+		}
+		
+		private void Update()
+		{
+			if (zoom)
+			{
+				_camera.orthographicSize -= App.levelData.CameraZoomInMaxValue * Time.unscaledDeltaTime;
+			}
+		}
+		
+		private void LateUpdate()
+		{
+			if (!runScript)
+				return;
+			
+			Vector3 desiredPosition = _target.position + _offset;
+			Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.smoothDeltaTime * _cameraSmoothSpeed);
+			transform.position = new Vector3(smoothedPosition.x, transform.position.y, transform.position.z);
+		}
+
+		public override void Init()
+		{
+			runScript = false;
+			zoom = false;
+		}
+
+		public override void OnNotificationReceived(Notification notification, NotificationParam optionalObject = null)
+		{
+			switch (notification)
+			{
+				case Notification.LevelReadyToStart:
+					_target = App.player.transform;
+					runScript = true;
+					break;
+				
+				case Notification.GlassShatterJuice:
+					zoom = true;
+					break;
+				
+				case Notification.LevelOver:
+					zoom = false;
+					break;
+				
+				case Notification.PlayerHitDeadObstacle:
+					runScript = false;
+					break;
+			}
+		}
+	}
+}
---
+++ Assets/Sourav/Utilities/Scripts/Camera/Util/OrthographicUtilities.cs
@@ -0,0 +1,80 @@
+﻿using Sourav.Utilities.Scripts.Attributes;
+using UnityEngine;
+
+namespace Sourav.Utilities.Scripts.Camera.Util
+{
+	[RequireComponent(typeof(UnityEngine.Camera))]
+	public class OrthographicUtilities : MonoBehaviour
+	{
+
+		[SerializeField] private Transform Left;
+		[SerializeField] private Transform Right;
+		
+		
+		
+		[SerializeField]private Vector3 leftLimit;
+		public Vector3 LeftLimit
+		{
+			get { return leftLimit; }
+		}
+
+		[SerializeField]private Vector3 rightLimit;
+		public Vector3 RightLimit
+		{
+			get { return rightLimit; }
+		}
+
+		[SerializeField]private Vector3 height;
+		public Vector3 Height
+		{
+			get { return height; }
+		}
+
+		[SerializeField]private float width;
+		public float Width
+		{
+			get { return width; }
+		}
+
+		private UnityEngine.Camera camera;
+
+
+		private void Awake()
+		{
+			camera = GetComponent<UnityEngine.Camera>();
+			PopulateLimits();
+		}
+
+		private void LateUpdate()
+		{
+			PopulateLimits();
+		}
+
+		private void PopulateLimits()
+		{
+			float heightY = 2 * camera.orthographicSize;
+			var position = camera.transform.position;
+			height = new Vector3(position.x, position.y + heightY, position.z);
+			width = heightY * camera.aspect;
+			float halfWidth = width * 0.5f;
+			leftLimit = new Vector3(position.x - halfWidth, position.y, position.z);
+			rightLimit = new Vector3(position.x + halfWidth, position.y, position.z);
+			
+			SetupLimits();
+		}
+
+		[Button()]
+		public void SetupLimits()
+		{
+			if (Left != null)
+			{
+				Left.position = leftLimit;
+			}
+
+			if (Right != null)
+			{
+				Right.position = rightLimit;
+			}
+		}
+	}
+}
