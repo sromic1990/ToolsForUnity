fc2b50bb24454d6fc9c50c861b63dc7c3ad5bc54
Sourav Chatterjee <sromic@gmail.com>
2019-08-13 12:30:54 +0530

    [Update] Haptic Feedback Controller updated

--- Assets/Sourav/Engine/Core/ControllerRelated/HapticFeedbackController.cs
+++ Assets/Sourav/Engine/Core/ControllerRelated/HapticFeedbackController.cs
@@ -1,4 +1,5 @@
-﻿using Sourav.Engine.Core.NotificationRelated;
+﻿using _IdleArtistDots._Scripts.Experimental;
+using Sourav.Engine.Core.NotificationRelated;
 using Sourav.Engine.Editable.NotificationRelated;
 using UnityEngine;
 
@@ -6,8 +7,50 @@ namespace Sourav.Engine.Core.ControllerRelated
 {
 	public class HapticFeedbackController : Controller
 	{
+		#region ANDROID
+		public class HapticFeedbackManagerAndroid
+		{
+			#if UNITY_ANDROID && !UNITY_EDITOR
+                private int hapticFeedbackConstantsKey;
+                private AndroidJavaObject UnityPlayer;
+			#endif
+
+			public HapticFeedbackManagerAndroid()
+			{
+				#if UNITY_ANDROID && !UNITY_EDITOR
+					hapticFeedbackConstantsKey =
+					new AndroidJavaClass("android.view.HapticFeedbackConstants").GetStatic<int>("VIRTUAL_KEY");
+					UnityPlayer =
+					new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
+				#endif
+			}
+
+			public bool Execute()
+			{
+				#if UNITY_ANDROID && !UNITY_EDITOR
+					return UnityPlayer.Call<bool>("performHapticFeedback", hapticFeedbackConstantsKey);
+				#endif
+				return false;
+			}
+		}
+		private static HapticFeedbackManagerAndroid _mHapticFeedbackManager;
+
+		public static bool HapticFeedback()
+		{
+			if (_mHapticFeedbackManager == null)
+			{
+				_mHapticFeedbackManager = new HapticFeedbackManagerAndroid();
+			}
+
+			return _mHapticFeedbackManager.Execute();
+		}
+		#endregion
+
 		[SerializeField] private iOSHapticFeedback.iOSFeedbackType feedbackTypeSuccess;
 		[SerializeField] private iOSHapticFeedback.iOSFeedbackType feedbackTypeFailure;
+
+		[Space(10)] [Header("Android Haptic Feedback")] [SerializeField]
+		private AndroidHapticManager hapticFeedback;
 		public override void OnNotificationReceived(Notification notification, NotificationParam param = null)
 		{
 			if (!App.GetLevelData().IsVibrationOn)
@@ -17,11 +60,11 @@ namespace Sourav.Engine.Core.ControllerRelated
 			
 			switch (notification)
 			{
-				case Notification.HapticSuccess:
+				case Notification.HapticHeavy:
 					HapticForSubmitButtonSuccess();
 					break;
 				
-				case Notification.HapticFailure:
+				case Notification.HapticMild:
 					HapticForSubmitButtonFailure();
 					break;
 			}
@@ -29,21 +72,27 @@ namespace Sourav.Engine.Core.ControllerRelated
 
 		private void HapticForSubmitButtonFailure()
 		{
-#if UNITY_IOS
+			Debug.Log("Haptic FAILURE");
+			#if UNITY_IOS && !UNITY_EDITOR
 			if (iOSHapticFeedback.Instance.IsSupported())
 			{
 				iOSHapticFeedback.Instance.Trigger(feedbackTypeFailure);
 			}
-#endif
+			#elif UNITY_ANDROID && !UNITY_EDITOR
+			HapticFeedback();
+			#endif
 		}
 
 		private void HapticForSubmitButtonSuccess()
 		{
-			#if UNITY_IOS
+			Debug.Log("Haptic SUCCESS");
+			#if UNITY_IOS && !UNITY_EDITOR
 			if (iOSHapticFeedback.Instance.IsSupported())
 			{
 				iOSHapticFeedback.Instance.Trigger(feedbackTypeSuccess);
 			}
+			#elif UNITY_ANDROID && !UNITY_EDITOR
+			HapticFeedback();
 			#endif
 		}
 	}
