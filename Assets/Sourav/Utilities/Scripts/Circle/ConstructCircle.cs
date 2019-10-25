bf002d9105a2ddede5fdab3eadf9ac0c663d5676
Sourav Chatterjee <sromic@gmail.com>
2019-10-25 14:13:47 +0530

    [Addition] Circle draw utility Added

---
+++ Assets/Sourav/Utilities/Scripts/Circle/ConstructCircle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sourav.Utilities.Scripts.Circle
+{
+    public static class ConstructCircle
+    {
+        public static List<Vector3> ConstructXZ(float radiusOfCircle, int number, Vector3 startingPosition, float defaultY = 0.0f)
+        {
+            List<Vector3> positions = new List<Vector3>();
+            for (int i = 0; i < number; i++)
+            {
+                float angle = i * Mathf.PI * 2f / number;
+                positions.Add(startingPosition + new Vector3(Mathf.Cos(angle) * radiusOfCircle, defaultY, Mathf.Sin(angle) * radiusOfCircle));
+            }
+            return positions;
+        }
+        
+        public static List<Vector3> ConstructXY(float radiusOfCircle, int number, Vector3 startingPosition, float defaultZ = 0.0f)
+        {
+            List<Vector3> positions = new List<Vector3>();
+            for (int i = 0; i < number; i++)
+            {
+                float angle = i * Mathf.PI * 2f / number;
+                positions.Add(startingPosition + new Vector3(Mathf.Cos(angle) * radiusOfCircle, Mathf.Sin(angle) * radiusOfCircle, defaultZ));
+            }
+            return positions;
+        }
+        
+        public static List<Vector3> ConstructYZ(float radiusOfCircle, int number, Vector3 startingPosition, float defaultX = 0.0f)
+        {
+            List<Vector3> positions = new List<Vector3>();
+            for (int i = 0; i < number; i++)
+            {
+                float angle = i * Mathf.PI * 2f / number;
+                positions.Add(startingPosition + new Vector3(defaultX, Mathf.Cos(angle) * radiusOfCircle, Mathf.Sin(angle) * radiusOfCircle));
+            }
+            return positions;
+        }
+    }
+}
---
+++ Assets/Sourav/Utilities/Scripts/Circle/Create3DCircles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Sourav.Engine.Core.GameElementRelated;
+using UnityEngine;
+
+namespace Sourav.Utilities.Scripts.Circle
+{
+    public class Create3DCircles : MonoBehaviour
+    {
+        public static void CreateCircles(List<Vector3> points, GameObject circlePrefab, Transform parent, Vector3 parentPosition)
+        {
+            parent.position = parentPosition;
+            
+            for (int i = 0; i < points.Count; i++)
+            {
+                GameObject go = (GameObject)Instantiate(circlePrefab, points[i], Quaternion.identity);
+                go.transform.SetParent(parent);
+
+            }
+        }
+    }
+}
