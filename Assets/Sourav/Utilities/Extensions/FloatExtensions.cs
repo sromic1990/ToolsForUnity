40956ee22f15023a8e80533baf4341d71ace2209
Sourav Chatterjee <sromic@gmail.com>
2019-12-31 12:46:51 +0530

    Remap added to float extensions

--- Assets/Sourav/Utilities/Extensions/FloatExtensions.cs
+++ Assets/Sourav/Utilities/Extensions/FloatExtensions.cs
@@ -8,15 +8,17 @@ namespace Sourav.Utilities.Extensions
     {
         public static bool Between(this float f, float f_lower, float f_higher)
         {
-            if (f < f_higher && f > f_lower)
+            float higher = Mathf.Max(f_lower, f_higher);
+            float lower = Mathf.Min(f_lower, f_higher);
+            
+            if (f < higher && f > lower)
                 return true;
             else return false;
         }
         
-        public static float Round(this float f, int roundValue)
+        public static float Remap (this float value, float from1, float to1, float from2, float to2) 
         {
-            f = (float)System.Math.Round((double) f, roundValue);
-            return f;
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
     }
 }
