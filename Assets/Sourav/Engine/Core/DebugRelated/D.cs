ab53e581f97082cda1c6380aeea4773fbef8e5c5
Sourav Chatterjee <sromic@gmail.com>
2020-01-23 13:18:45 +0530

    Debug is shortened to D. Log On Off shortcuts added.

---
+++ Assets/Sourav/Engine/Core/DebugRelated/D.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Sourav.Engine.Core.DebugRelated
+{
+    public class D
+    {
+        public static void Log(object message)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.Log(message);
+            #endif
+        }
+
+        public static void Log(object message, Object context)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.Log(message, context);
+            #endif
+        }
+
+        public static void LogError(object message)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogError(message);
+            #endif
+        }
+
+        public static void LogError(object message, Object context)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogError(message, context);
+            #endif
+        }
+
+        public static void LogException(Exception exception)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogException(exception);
+            #endif
+        }
+        
+        public static void LogException(Exception exception, Object context)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogException(exception, context);
+            #endif
+        }
+
+        public static void LogWarning(object message)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogWarning(message);
+            #endif
+        }
+        
+        public static void LogWarning(object message, Object context)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogWarning(message, context);
+            #endif
+        }
+
+        public static void LogAssertion(object message)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogAssertion(message);
+            #endif
+        }
+        
+        public static void LogAssertion(object message, Object context)
+        {
+            #if LOG || UNITY_EDITOR
+            Debug.LogAssertion(message, context);
+            #endif
+        }
+    }
+}
---
+++ Assets/Sourav/Utilities/Editor/DebugRelated/AddRemoveDebugFromBuild.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace Sourav.Utilities.Editor.DebugRelated
+{
+    public class AddRemoveDebugFromBuild : UnityEditor.Editor
+    {
+        private static List<string> symbols;
+        
+        [MenuItem("ProjectUtility/Utilities/Add Log %#l")]
+        public static void AddLog()
+        {
+            GetScriptingSymbols();
+            if (!symbols.Contains("LOG"))
+            {
+                symbols.Add("LOG");
+            }
+            ResetDefineSymbols();
+        }
+
+        [MenuItem("ProjectUtility/Utilities/Remove Log %l")]
+        public static void RemoveLog()
+        {
+            GetScriptingSymbols();
+            
+            List<string> newSymbols = new List<string>();
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                if (symbols[i] == "LOG")
+                {
+                    continue;
+                }
+                
+                newSymbols.Add(symbols[i]);
+            }
+
+            symbols = newSymbols;
+            ResetDefineSymbols();
+        }
+        
+        private static void GetScriptingSymbols()
+        {
+            symbols = new List<string>();
+
+            string defines =
+                PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+
+            symbols = defines.Split(';').ToList();
+        }
+
+        private static void ResetDefineSymbols()
+        {
+            string defineSymbols = "";
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                defineSymbols += symbols[i] + ";";
+            }
+
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+                defineSymbols);
+        }
+    }
+}
