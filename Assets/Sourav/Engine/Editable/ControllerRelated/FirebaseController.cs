dd7c55f3e0c11540d16afd8f4f1cc164b3caa40a
Sourav Chatterjee <sromic@gmail.com>
2020-01-28 19:15:55 +0530

    Firebase controller added to Controllers.

---
+++ Assets/Sourav/Engine/Editable/ControllerRelated/FirebaseController.cs
@@ -0,0 +1,35 @@
+﻿using Sourav.Engine.Core.ControllerRelated;
+using Sourav.Engine.Core.NotificationRelated;
+using Sourav.Engine.Editable.NotificationRelated;
+
+namespace Sourav.Engine.Editable.ControllerRelated
+{
+    public class FirebaseController : Controller
+    {
+        void Start()
+        {
+            Firebase.Analytics.FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        }
+
+        public override void OnNotificationReceived(Notification notification, NotificationParam param = null)
+        {
+            switch (notification)
+            {
+                case Notification.LevelComplete:
+                    Firebase.Analytics.FirebaseAnalytics
+                        .LogEvent("StageComplete "+App.GetLevelData().CurrentLevel);
+                    break;
+                
+                case Notification.AdRewarded:
+                    Firebase.Analytics.FirebaseAnalytics
+                        .LogEvent("RewardVideoSeen");
+                    break;
+                
+                //case Notification.InterstitialClosed:
+                //    Firebase.Analytics.FirebaseAnalytics
+                //        .LogEvent("InterstitialShown");
+                //    break;
+            }
+        }
+    }
+}
