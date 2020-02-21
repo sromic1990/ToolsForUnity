a066e1246a283fdfd8442f8ad15457ac75bc0e17
Sourav Chatterjee <sromic@gmail.com>
2020-02-21 17:49:51 +0530

    [Update] Curve Reader and test added.

---
+++ Assets/Sourav/Engine/Editable/CurveRelated/CurveReading.cs
@@ -0,0 +1,21 @@
+﻿using Sourav.Engine.Core.DebugRelated;
+using Sourav.Engine.Core.GameElementRelated;
+using UnityEngine;
+
+namespace Sourav.Engine.Editable.CurveRelated
+{
+    public class CurveReading : GameElement
+    {
+        [SerializeField] private AnimationCurve curve;
+
+        [Sirenix.OdinInspector.Button()]
+        public float GetValue(float timeOfCurve)
+        {
+            timeOfCurve = Mathf.Clamp01(timeOfCurve);
+            float result = curve.Evaluate(timeOfCurve);
+            D.Log($"Value = {result}");
+            
+            return result;
+        }
+    }
+}
---
+++ Assets/Sourav/Test/CurveReadingTest/_Scripts/CurveValueTest.cs
@@ -0,0 +1,25 @@
+﻿using Sourav.Engine.Core.DebugRelated;
+using Sourav.Engine.Core.GameElementRelated;
+using Sourav.Engine.Editable.CurveRelated;
+using Sourav.Utilities.Extensions;
+using UnityEngine;
+
+namespace Sourav.Test.CurveReadingTest._Scripts
+{
+    public class CurveValueTest : GameElement
+    {
+        [SerializeField] private CurveReading curve;
+        [SerializeField] private Vector2 values;
+
+        [Sirenix.OdinInspector.Button()]
+        private void GetValueAt(float time)
+        {
+            time = Mathf.Clamp01(time);
+            float value = curve.GetValue(time);
+
+            float finalValue = value.Remap(0, 1, values.x, values.y);
+            
+            D.Log($"finalValue = {finalValue}");
+        }
+    }
+}
