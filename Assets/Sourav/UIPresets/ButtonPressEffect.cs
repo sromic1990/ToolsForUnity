57a61f0d592af974e2a2d6988b3278cc1c4ea586
Sourav Chatterjee <sromic@gmail.com>
2020-03-07 12:54:02 +0530

    Button Press effect script added to UI Presets

---
+++ Assets/Sourav/UIPresets/ButtonPressEffect.cs
@@ -0,0 +1,77 @@
+﻿using Sirenix.OdinInspector;
+using Sourav.Engine.Core.GameElementRelated;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Sourav.UIPresets
+{
+    public class ButtonPressEffect : GameElement, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private GameObject pushDownObject;
+        [SerializeField] private Vector3 onPointerDownPosition;
+        [SerializeField] private Vector3 onPointerUpPosition;
+
+        [SerializeField] private bool isLocal;
+        [SerializeField] private bool isChangeImage;
+
+        [ShowIf("isChangeImage", true)] [SerializeField]
+        private Image image;
+
+        [ShowIf("isChangeImage", true)] [SerializeField]
+        private Sprite pressUpImage;
+
+        [ShowIf("isChangeImage", true)] [SerializeField]
+        private Sprite pressDownImage;
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (isChangeImage)
+            {
+                if (image != null)
+                {
+                    if (pressDownImage != null)
+                    {
+                        image.sprite = pressDownImage;
+                    }
+                }
+            }
+            else
+            {
+                if (isLocal)
+                {
+                    pushDownObject.transform.localPosition = onPointerDownPosition;
+                }
+                else
+                {
+                    pushDownObject.transform.position = onPointerDownPosition;
+                }
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (isChangeImage)
+            {
+                if (image != null)
+                {
+                    if (pressUpImage != null)
+                    {
+                        image.sprite = pressUpImage;
+                    }
+                }
+            }
+            else
+            {
+                if (isLocal)
+                {
+                    pushDownObject.transform.localPosition = onPointerUpPosition;
+                }
+                else
+                {
+                    pushDownObject.transform.position = onPointerUpPosition;
+                }
+            }
+        }
+    }
+}
