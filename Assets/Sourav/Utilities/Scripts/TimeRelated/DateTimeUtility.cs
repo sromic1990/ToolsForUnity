d16ccbb725369334b01bab9bfc73639b83aff636
Sourav Chatterjee <sromic@gmail.com>
2020-11-12 18:23:04 +0530

    Added DateTime Utility

---
+++ Assets/Sourav/Utilities/Scripts/TimeRelated/DateTimeUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sourav.Utilities.Scripts.TimeRelated
+{
+    public static class DateTimeUtility
+    {
+        public static int EvaluateSeconds(string dateTime)
+        {
+            DateTime now = DateTime.Now;
+
+            DateTime last = DateTime.Parse(dateTime, CultureInfo.InvariantCulture);
+
+            TimeSpan span = now - last;
+
+            // D.Log("span seconds = "+span.Seconds);
+            // D.Log("span minutes = "+span.Minutes);
+            // D.Log("span hours = "+span.Hours);
+            // D.Log("span days = "+span.Days);
+
+            int totalSeconds = span.Seconds + (span.Minutes * 60) + (span.Hours * 60 * 60) + (span.Days * 24 * 60 * 60);
+            
+            return totalSeconds;
+        }
+    }
+}
